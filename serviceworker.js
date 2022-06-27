a97e58b107737e57cbe896ae761ae14a281795e8
Ron Spierings <ro.spierings@kw1c.nl>
2022-06-27 22:42:32 +0200

    Still caching problems?

--- serviceworker.js
+++ serviceworker.js
@@ -11,7 +11,7 @@ const WORKBOX_DEBUG_LOGGING = false;
 const WORKBOX_VERSION = '5.1.1';
 
 
-const staticCacheName = 'music-cache-v3.1';
+const staticCacheName = 'music-cache-v3.2';
 
 const baseUrl = 'https://r-spierings.nl/'; 
 
@@ -39,21 +39,29 @@ self.addEventListener('install', function(event) {
         broadcast.postMessage({type: 'CACHE_START_DOWNLOADING'});
 
         // Add all the cache files from the statics
-        cache.addAll(filesToCache);
+
+        // cache.addAll(filesToCache);
 
         // Next, download every sound location and Cache every sound file
         fetch('https://r-spierings.nl/AudioTourOssAdmin/api/collections/get/SoundLocation?token=bb9d57d773bcc3e75e1347f43b5d48')
         .then(response => {
           return response.json();
         })
-        .then(responseJson => {          
+        .then(responseJson => {    
+          
           if(responseJson.entries != undefined)
           {
             for(let item of responseJson.entries)
             {
               console.log("Adding to cache:", baseUrl + item.mp3file);
-              cache.add(baseUrl + item.mp3file);
-            }      
+              
+              // Add this file to the current fileToCache list
+              filesToCache.push(baseUrl + item.mp3file);              
+            } 
+
+            console.log("Current to be cached list:" , filesToCache);
+            
+            cache.addAll(filesToCache);     
             broadcast.postMessage({type: 'CACHE_COMPLETED'});        
           }
         })
